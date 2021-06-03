749e2b9ad52319f91ee504a34303e9dc41ecbbd2
Saif Hasnaoui <saif.hasnaoui@ai4bd.com>
2021-06-03 17:17:02 +0100

    add: re-initiate the project.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'IAM-Frontend'`, () => {
+  it(`should have as title 'IAM'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('IAM-Frontend');
+    expect(app.title).toEqual('IAM');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('IAM-Frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('IAM app is running!');
   });
 });
